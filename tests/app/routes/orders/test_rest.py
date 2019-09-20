01844af68e98ee66dbe5825a2c285db53a841907
kenlt-uk <kenlt.uk@gmail.com>
2019-09-20 01:58:41 +0100

    Fix tests when creating an order

--- tests/app/routes/orders/test_rest.py
+++ tests/app/routes/orders/test_rest.py
@@ -25,19 +25,19 @@ sample_ipns = [
     "item_number2={id}&payer_id=XXYYZZ2&address_street=Flat+1%2C+70+Angel+Place&payment_date=14%3A45%3A55+Mar+"
     "30%2C+2019+PDT&option_name2_1=Course+Member+name&option_name2_2=Course+Member+name&option_selection1_1=Full&"
     "payment_status=Completed&option_selection1_2=Full&charset=windows-1252&address_zip=n1+1xx&mc_shipping=0.00&"
-    "first_name=Timothy&mc_fee=0.54&address_country_code=GB&address_name=Test+User&notify_version=3.9&custom=&"
+    "first_name=Test&mc_fee=0.54&address_country_code=GB&address_name=Test+User&notify_version=3.9&custom=&"
     "payer_status=unverified&business=receiver%40example.com&address_country=United+Kingdom&num_cart_items=2&"
     "mc_handling1=0.00&mc_handling2=0.00&address_city=London&verify_sign="
     "AUl-112233&payer_email=test2%40example.com&btn_id1=XXYYZZ1&"
     "btn_id2=XXYYZZ2&option_name1_1=Type&option_name1_2=Type&txn_id={txn_id}&payment_type=instant&"
-    "option_selection2_1=-&last_name=Hague&address_state=&option_selection2_2=-&item_name1=Philosophy+of+World&"
+    "option_selection2_1=-&last_name=User&address_state=&option_selection2_2=-&item_name1=Philosophy+of+World&"
     "receiver_email=receiver%40example.com&item_name2=The+Mystery+Behind+the+Brain&payment_fee=&"
     "shipping_discount=0.00&quantity1=1&insurance_amount=0.00&quantity2=1&receiver_id=112233&txn_type={txn_type}&"
     "discount=0.00&mc_gross_1=5.00&mc_currency=GBP&mc_gross_2=5.00&residence_country=GB&receipt_id=112233"
     "&shipping_method=Default&transaction_subject=&payment_gross=&ipn_track_id=112233",
     # paypal card reader
     "cmd=_notify-validate&mc_gross=24.00&protection_eligibility=Ineligible&payer_id=XXYYZZ3&tax=0.00&"
-    "payment_date=15%3A27%3A52+Jan+01%2C+2018+PST&payment_status=Completed&payment_method=credit_card&"
+    "payment_date=19%3A27%3A52+Jan+01%2C+2018+PST&payment_status=Completed&payment_method=credit_card&"
     "invoice_id=INV2-XXYYZZ&charset=windows-1252&first_name=&mc_fee=0.66&notify_version=3.9&"
     "custom=%5BCONTACTLESS_CHIP%28V%2C7102%29%40%2851.112233%2C-0."
     "112233%29%2C%2819290223020%29%5D&payer_status=unverified&"
@@ -108,6 +108,8 @@ class WhenHandlingPaypalIPN:
         assert mock_post.call_args[0][0] == url
 
     def it_creates_orders_and_tickets(self, mocker, client, db_session, sample_event_with_dates):
+        mocker.patch('app.routes.orders.rest.Storage')
+        mocker.patch('app.routes.orders.rest.Storage.upload_blob_from_base64string')
         txn_ids = ['112233', '112244', '112255']
         txn_types = ['cart', 'cart', 'paypal_here']
         num_tickets = [1, 2, 1]
