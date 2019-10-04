b3efc3bde07e4b1987a10a625939e795b3692c48
kenlt-uk <kenlt.uk@gmail.com>
2019-10-05 00:21:18 +0100

    wip

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -7,14 +7,22 @@ if [ ! -z "$1" ]; then
     ENV=$1
 fi
 
+if [ "$ENV" = 'preview' ]; then
+    PORT=4555
+else
+    PORT=5555
+fi
+
 if [ -z "$VIRTUAL_ENV" ] && [ -d venv ]; then
   echo 'activate venv for celery'
   source ./venv/bin/activate
   nooutput=' >&- 2>&- <&- &'
 fi
 
+pip install flower==0.9.3
+
 # kill existing celery workers
 ps auxww | grep "celery worker-$ENV" | awk '{print $2}' | xargs kill -9
 
 eval "celery -A run_celery.celery worker --loglevel=INFO -n worker-$ENV@%h --concurrency=1"$nooutput
-eval "celery -A run_celery.celery flower"$nooutput
+eval "celery -A run_celery.celery flower --url_prefix=celery --address=127.0.0.1 --port=$PORT"$nooutput
